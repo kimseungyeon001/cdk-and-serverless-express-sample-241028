37cb6f7cdbccf519a1a510036bc8e7d4fd672702
kimseungyeon001 <kim.seungyeon@classmethod.jp>
2024-10-28 20:49:55 +0900

    improvement packages-server

--- packages/server/src/lambda/api-gateway/rest-api/router.ts
+++ packages/server/src/lambda/api-gateway/rest-api/router.ts
@@ -1,12 +1,12 @@
-import serverlessExpress from '@codegenie/serverless-express';
-import cors from 'cors';
-import express, { Request, Response } from 'express';
+import serverlessExpress from "@codegenie/serverless-express";
+import cors from "cors";
+import express, { Request, Response } from "express";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (_req: Request, res: Response): Promise<void> => {
+app.get("/", async (_req: Request, res: Response): Promise<void> => {
   res.status(200).send("Hello serverless express!");
 });
 
